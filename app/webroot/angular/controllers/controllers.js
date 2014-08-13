eb2b71b163d1d61cadcbadb1f8c8e2f92022ad82
Martin Rabaglia <tinchoforever@gmail.com>
2014-08-13 09:10:21 -0300

    usando autocompletar de organizaciones con angularjs

--- app/webroot/angular/controllers/controllers.js
+++ app/webroot/angular/controllers/controllers.js
@@ -2,20 +2,25 @@
 
 /* Controllers */
 angular.module('cargoApp.controllers')
-  .controller('homeController', function($scope, $http, searchProvider) {
-  	$scope.posts = ["Gobernador"];
-
-    $scope.fetchPosts = function(typed){
-        $scope.latestPost = searchProvider.fetchPositions(typed);
-        $scope.latestPost.then(function(data){
-        	console.log(data);
-          $scope.posts = data.data.result;
-        });
-    }
+  .controller('homeController', function($scope, $http,$filter) {
+  	$scope.organizations = [];
+  	$scope.rawOrganizations = [];
 
     $http.get('/js/gz/cargografias-organizations-popit-dump.json')
        .then(function(res){
-          console.log(res.data);
+       	$scope.organizations = res.data;
+	       	for (var i = 0; i < res.data.length; i++) {
+	       		$scope.rawOrganizations.push({
+		       			id : res.data[i].id,
+		       			name : res.data[i].name
+	       			});
+	       	};
         });
 
+    $scope.handleSelection = function(id){
+    	console.log(id);
+    	$scope.selectedOrganization =$scope.organizations[id];
+    	console.log($scope.selectedOrganization);
+    }
   });
+
