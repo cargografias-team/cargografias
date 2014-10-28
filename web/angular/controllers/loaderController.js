774e170aebd3b56faaa4747f6d167893f42b364d
Martin Rabaglia <tinchoforever@gmail.com>
2014-10-27 21:57:39 -0300

    agrego loadercontroller.js

---
+++ web/angular/controllers/loaderController.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/* Controllers */
+angular.module('cargoApp.controllers')
+  .controller('loaderController', function($rootScope,$q,
+   $scope,cargosFactory, presetsFactory, $filter,$cookies, $routeParams, $location, $route, $timeout) {
+   
+ });
